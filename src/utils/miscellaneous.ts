0b4796a5ac201f71271c2222a142058b90f4d642
Stepan Krousky <stepan.krousky@seznam.cz>
2024-09-23 11:34:38 +0200

    edit debounce

--- src/utils/miscellaneous.ts
+++ src/utils/miscellaneous.ts
@@ -8,14 +8,28 @@
  * @example
  * const debouncedLog = debounce(console.log, 200);
  * debouncedLog('Hello!'); // Will log 'Hello!' only after 200 ms if not called again within this time.
+ * @example
+ * const sum = (a: number, b: number): number => a + b;
+ * const debouncedSum = debounce(sum, 100);
+ * const runDebounce = async () => {
+ *   const result = await debouncedSum(1, 2); // Call the debounced function
+ *   console.log(result); // Logs the result of the sum
+ * };
+ * runDebounce();
  */
-export const debounce = <T extends (...args: any[]) => void>(
+export const debounce = <T extends (...args: any[]) => any>(
   func: T,
   delay: number
-): ((...args: Parameters<T>) => void) => {
+): ((...args: Parameters<T>) => Promise<ReturnType<T> | undefined>) => {
   let timeoutId: ReturnType<typeof setTimeout>
-  return (...args: Parameters<T>): void => {
+
+  return (...args: Parameters<T>): Promise<ReturnType<T> | undefined> => {
     clearTimeout(timeoutId)
-    timeoutId = setTimeout(() => func(...args), delay)
+    return new Promise((resolve) => {
+      timeoutId = setTimeout(() => {
+        const result = func(...args)
+        resolve(result)
+      }, delay)
+    })
   }
 }
